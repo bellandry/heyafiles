5a9096a259581e29297596015dcb1d73975e55b9
bellandry <bellandry.work@gmail.com>
2025-11-23 09:59:50 +0100

    update: display green lignt when login on web frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import { SearchInput } from "@/components/search-input";
 import { FileItem } from "@/types/file";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import { toast } from "sonner";
 
 const API_URL = "http://localhost:3002/files";
@@ -15,6 +15,7 @@ const SOCKET_URL = "http://localhost:3002";
 export default function Home() {
   const [files, setFiles] = useState<FileItem[]>([]);
   const [filteredFiles, setFilteredFiles] = useState<FileItem[]>([]);
+  const [socket, setSocket] = useState<Socket | null>(null);
   const [loading, setLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -36,6 +37,7 @@ export default function Home() {
 
     // Connect to Socket.io
     const socket = io(SOCKET_URL);
+    setSocket(socket);
 
     socket.on("connect", () => {
       console.log("connectet to websocket");
@@ -71,15 +73,20 @@ export default function Home() {
   }, [searchQuery, files]);
 
   return (
-    <main className="min-h-screen p-4 sm:p-8 font-sans w-full bg-gray-50">
-      <div className="max-w-4xl mx-auto space-y-8">
+    <main className="p-4 w-full min-h-screen font-sans bg-gray-50 sm:p-8">
+      <div className="mx-auto space-y-8 max-w-4xl">
         {/* Simple Header */}
-        <div className="flex items-center justify-between">
+        <div className="flex justify-between items-center">
           <div className="-space-y-1">
-            <h1 className="text-xl md:text-2xl font-bold text-violet-900">
+            <h1 className="flex gap-2 items-center text-xl font-bold text-violet-900 md:text-2xl">
               HeyaFiles
+              <div
+                className={`size-2 rounded-full ${
+                  socket?.connected ? "bg-green-500 animate-ping" : "bg-red-500"
+                }`}
+              />
             </h1>
-            <p className="text-slate-500 text-xs">Simple files storage</p>
+            <p className="text-xs text-slate-500">Simple files storage</p>
           </div>
           <div className="flex gap-4">
             <div className="hidden md:block">
@@ -92,8 +99,8 @@ export default function Home() {
             <UploadForm />
           </div>
         </div>
-        <div className="space-y-2 py-10">
-          <h1 className="text-3xl sm:text-4xl font-bold tracking-tight text-violet-950">
+        <div className="py-10 space-y-2">
+          <h1 className="text-3xl font-bold tracking-tight sm:text-4xl text-violet-950">
             Your Image Gallery
           </h1>
           <p className="text-slate-500">
@@ -103,11 +110,11 @@ export default function Home() {
 
         <div className="space-y-6">
           {loading ? (
-            <div className="text-center py-16 text-slate-400">
+            <div className="py-16 text-center text-slate-400">
               Chargement...
             </div>
           ) : (
-            <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4  gap-4">
+            <div className="grid grid-cols-2 gap-4 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
               <div className="relative col-span-2 md:hidden">
                 <SearchInput
                   value={searchQuery}
@@ -127,7 +134,7 @@ export default function Home() {
           )}
 
           {!loading && filteredFiles.length === 0 && (
-            <div className="text-center space-y-6 py-16 border-2 border-dashed rounded-xl bg-white shadow-sm">
+            <div className="py-16 space-y-6 text-center bg-white rounded-xl border-2 border-dashed shadow-sm">
               <p className="text-md text-slate-500">
                 {searchQuery
                   ? "Aucune correspondance pour votre recherche"
